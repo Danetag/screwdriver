aa02fafe0315d1ad92b2f14087571a7af1dbf7f9
Danetag <arno.tanielian@gmail.com>
2015-06-26 15:25:30 -0400

    fix lodash issue + config gulp

--- slushfile.js
+++ slushfile.js
@@ -21,6 +21,7 @@ gulp.task('default', function (done) {
     gulp.src([
         __dirname + '/root/**',
         '!' + __dirname + '/root/app/static/**',  // Exclude images to avoid Loadash issues 
+        '!' + __dirname + '/root/app/scripts/vendor/**',  // Exclude vendros to avoid Loadash issues 
       ])  // Note use of __dirname to be relative to generator
       .pipe(template(answers))        // Lodash template support
       .pipe(gulp.dest('./'))          // Without __dirname here = relative to cwd
@@ -30,6 +31,10 @@ gulp.task('default', function (done) {
         // Copy images
         gulp.src(__dirname + '/root/app/static/**')
             .pipe(gulp.dest('./app/static'))
+
+        // Copy vendor
+        gulp.src(__dirname + '/root/app/scripts/vendor/**')
+            .pipe(gulp.dest('./app/scripts/vendor'))
             .on('finish', function () {
               done();                       // Finished!
             })
